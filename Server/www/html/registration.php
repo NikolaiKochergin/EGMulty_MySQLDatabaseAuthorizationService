4bfc19067a1ca76b4726659a421bb24f83dfc7c4
Nikolai Kochergin <Kocherginizer@gmail.com>
2024-05-03 21:57:59 +0300

    Add cards creation in SQL db.

--- Server/www/html/registration.php
+++ Server/www/html/registration.php
@@ -20,6 +20,21 @@
     $user -> login = $login;
     $user -> password = $password;
 
+    $availableCards = array(1, 2, 3, 4, 5, 6);
+    foreach($availableCards as $id){
+        $card = R::load('cards', $id);
+        $user -> link('cards_users', array('selected' => false)) -> cards = $card;
+    }
+    
+    R::store($user);
+
+    $selectedID = array(1, 3, 5, 6);
+    $links = $user -> withCondition('cards_users.cards_id IN ('. R::genSlots($selectedID) .')', $selectedID) -> ownCardsUsers;
+
+    foreach($links as $link){
+        $link -> selected = true;
+    }
+
     R::store($user);
 
     echo 'ok';
